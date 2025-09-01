ad0432deedafd5e7dac5d44addce9ebf77cc50e8
Yafet D <japhethdanny@gmail.com>
2025-09-01 10:39:09 +0300

    Removed the templates and static dirs, moreover added all the endpoints to the root url

--- users/views.py
+++ users/views.py
@@ -4,17 +4,32 @@ from .serializers import UserProfileSerializer, UserProfileUpdateSerializer
 from .models import UserProfile
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.urls import reverse
 
 @api_view(['GET'])
 def api_root(request, format=None):
+    """
+    Root API endpoint listing all accessible URLs dynamically.
+    """
     return Response({
         'message': 'Welcome to GymLog API',
         'status': 'OK',
         'version': '1.0.0',
         'endpoints': {
-            'users': request.build_absolute_uri('api/users/'),
-            'workouts': request.build_absolute_uri('api/workouts/'),
-            'admin': request.build_absolute_uri('admin/'),
+            # Users app
+            'user_registration': request.build_absolute_uri(reverse('user-register')),
+            'user_login': request.build_absolute_uri(reverse('user-login')),
+            'user_profile': request.build_absolute_uri(reverse('user-profile')),
+
+            # Workouts app
+            'workouts_list_create': request.build_absolute_uri(reverse('workout-list-create')),
+            'workout_detail_example': request.build_absolute_uri(reverse('workout-detail', kwargs={'pk': 1})),
+            'categories_list_create': request.build_absolute_uri(reverse('category-list-create')),
+            'category_detail_example': request.build_absolute_uri(reverse('category-detail', kwargs={'pk': 1})),
+            'analytics_generate': request.build_absolute_uri(reverse('analytics-generate')),
+
+            # Admin
+            'admin': request.build_absolute_uri(reverse('admin:index')),
         }
     })
 
